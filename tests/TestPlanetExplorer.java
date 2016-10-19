42069806b39af4298ae331d1b9121dce17226639
somename <someemail>
2016-10-19 15:02:32 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -3,10 +3,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
-	PlanetExplorer pe=new PlanetExplorer(100,100,"");
+	
 	@Test
 	public void test_createPlanet_100_100() {
-		
+		PlanetExplorer pe=new PlanetExplorer(100,100,"");
 		assertEquals( "X=100", 100, pe.getGridx());
 		assertEquals( "Y=100", 100, pe.getGridy());
 		
@@ -14,20 +14,30 @@ public class TestPlanetExplorer {
 
 	@Test
 	public void test_execute_EmptyCommand() {
+		PlanetExplorer pe=new PlanetExplorer(100,100,"");
 		String status = pe.executeCommand("");
 		assertEquals("Status should be 0,0 facing north","0,0,N",status);
 	}
 
 	@Test
 	public void test_execute_CommandRight() {
+		PlanetExplorer pe=new PlanetExplorer(100,100,"");
 		String status = pe.executeCommand("r");
 		assertEquals("Status should be 0,0 facing east","0,0,E",status);
 	}
 	
 	@Test
 	public void test_execute_CommandLeft() {
+		PlanetExplorer pe=new PlanetExplorer(100,100,"");
 		String status = pe.executeCommand("l");
 		assertEquals("Status should be 0,0 facing west","0,0,W",status);
+	}
+
+	@Test
+	public void test_execute_CommandForward() {
+		PlanetExplorer pe=new PlanetExplorer(7,6,"");
+		String status = pe.executeCommand("f");
+		assertEquals("Status should be 7,7 facing North","7,7,W",status);
 	}
 
 	/*
