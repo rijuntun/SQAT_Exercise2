0b37e9daed88bac86aa9d7e401df2b508c1e04da
somename <someemail>
2016-10-19 14:39:11 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -7,8 +7,9 @@ public class TestPlanetExplorer {
 	@Test
 	public void test_createPlanet_100_100() {
 		PlanetExplorer pe=new PlanetExplorer(100,100,"");
-		assertEquals(100,pe.x);
-		assertEquls(100, pe.y);
+		assertEquals( "X=100", 100, pe.x);
+		assertEquals( "Y=100", 100, pe.y);
+		
 	}
 	/*
 	@Test
