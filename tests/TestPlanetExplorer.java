57b8eaa204623112b11f5c5772d1d9aa15423253
somename <someemail>
2016-10-19 14:48:07 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -3,14 +3,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
-
+	PlanetExplorer pe=new PlanetExplorer(100,100,"");
 	@Test
 	public void test_createPlanet_100_100() {
-		PlanetExplorer pe=new PlanetExplorer(100,100,"");
+		
 		assertEquals( "X=100", 100, pe.getGridx());
 		assertEquals( "Y=100", 100, pe.getGridy());
 		
 	}
+
+	@Test
+	public void test_execute_EmptyCommand() {
+		String status = pe.executeCommand("");
+		assertEquals("Status should be 0,0 facing north","0,0,N",status);
+	}
+
 	/*
 	@Test
 	public void test_executeCommand() {
